2d3b6a3651cabb0d27a878ef78bc45b0b77602ce
Abraham Shimekt <shimektabraham@gmail.com>
2021-12-27 00:43:10 +0300

    my first commit

---
+++ fizzBuzz.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer=[]
+        for i in range(n):
+            if (i+1)%3 ==0 and (i+1)%5 ==0:
+                answer.append("FizzBuzz")
+            elif (i+1)%3 ==0:
+                answer.append("Fizz")
+            elif (i +1)%5 ==0:
+                answer.append("Buzz")
+            else:
+                answer.append(str(i + 1))
+        return answer
