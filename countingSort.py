e84f12e79ca942fbfb035909b2e52a5f20d21193
Abraham Shimekt <shimektabraham@gmail.com>
2021-12-28 19:54:59 +0300

    sortingCount

---
+++ countingSort.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/countingsort1/problem
+def countingSort(arr):
+    n = len()
+    farr = [0]*(n)
+    for j in arr:
+        farr[j] +=1
+    return farr
+        
