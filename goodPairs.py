05cb32d4558a8048bca3094bad8cc3944dd239fd
Abraham Shimekt <shimektabraham@gmail.com>
2022-01-10 00:01:27 +0300

    good pair

---
+++ goodPairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        counter = 0
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j]:
+                    counter +=1
+        return counter
