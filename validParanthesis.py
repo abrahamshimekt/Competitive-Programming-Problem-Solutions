c3345719813b4f6330be7b0b2c01456e77a976a6
abe <shimektabraham@gmail.com>
2022-02-25 00:55:55 +0300

    valid paranthesis

---
+++ validParanthesis.py
@@ -0,0 +1,10 @@
+def isValid(sequence):
+    while True:
+        if '()' in sequence:
+            sequence = sequence.replace('()','')
+        elif '()' in sequence:
+            sequence = sequence.replace('()','')
+        elif '()' in sequence:
+            sequence = sequence.replace('()','')
+        else:
+            return not sequence
