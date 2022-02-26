8e37a370eafc5241e6a3df1048a9e7d40732ad9e
abe <shimektabraham@gmail.com>
2022-02-26 16:52:38 +0300

    pancake sorting

---
+++ panCakeSorting.py
@@ -0,0 +1,15 @@
+def pancakeSort(arr):
+    outPut = []
+    if sorted(arr) ==arr:
+        return outPut
+    else:
+        target = sorted(arr)
+        for i in range(len(arr)-1,-1,-1):
+            pivot = arr.index(target[i])
+            if pivot != i:
+                outPut.append(pivot + 1)
+                arr = arr[pivot::-1] + arr[pivot +1:]
+                outPut.append(i + 1)
+                arr = arr[i::-1] + arr[i+1:]
+        return outPut
+                    
