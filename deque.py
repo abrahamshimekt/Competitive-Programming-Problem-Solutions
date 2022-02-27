039f7a49dab36e6731b2bc840a212c2e5072268a
abe <shimektabraham@gmail.com>
2022-02-27 20:55:33 +0300

    deque(double ended queue implementation using circular array

---
+++ deque.py
@@ -0,0 +1,58 @@
+from collections import deque
+
+
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.arr = deque(maxlen = k)
+    def insertFront(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.arr.appendleft(value)
+        return True
+    def insertLast(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.arr.append(value)
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.arr.popleft()
+        return True
+
+    def deleteLast(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.arr.pop()
+        return True
+    def getFront(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.arr[0]
+        
+
+    def getRear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.arr[-1] 
+    
+    def isEmpty(self) -> bool:
+        return len(self.arr) == 0 
+        
+    def isFull(self) -> bool:
+        return len(self.arr) == self.arr.maxlen
+        
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
