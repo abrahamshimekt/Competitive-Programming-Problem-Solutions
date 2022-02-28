523ddcc27e27a4a0ae1dd95e929a8501f9e6597a
abe <shimektabraham@gmail.com>
2022-02-28 20:46:11 +0300

    top k

--- topKFrequent.py
+++ topKFrequent.py
@@ -1,9 +1,11 @@
-from ast import List
 from collections import Counter
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums,k):
         sortedNum= sorted(Counter(nums).items(), key = lambda x:x[1], reverse = True)
         outPut =[]
         for i in range(k):
             outPut.append(sortedNum[i][0])
         return outPut
+if __name__=="__main__":
+    s = Solution()
+    print(s.topKFrequent([2,3,5,6,8,7,7,8,1,3],3))
