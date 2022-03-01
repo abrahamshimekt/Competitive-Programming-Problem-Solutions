f61cb6fb5e2cde43e1c980ee5898e3d8790b7dd3
abe <shimektabraham@gmail.com>
2022-03-01 13:46:17 +0300

    middle node

--- middle.py
+++ middle.py
@@ -3,11 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from tkinter.tix import ListNoteBook
+from typing import Optional
+
+
 class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = head 
-        fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        return slow
+    def middleNode(self, head: Optional[ListNoteBook]) -> Optional[ListNode]:
+        curr = head
+        length  = 0
+        while curr:
+            length +=1
+            curr = curr.next
+        curr  = head
+        for i in range(length//2):
+            curr = curr.next
+        return curr
+
+        
+        # slow = head 
+        # fast = head
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+        # return slow
