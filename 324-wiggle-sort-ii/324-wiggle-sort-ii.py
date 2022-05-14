33fcdb916567ae8eaf762819fffd07edf6ce4ecd
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-05-14 11:46:08 +0300

    Time: 276 ms (35.92%), Space: 17.2 MB (16.55%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.py
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.py
@@ -4,9 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         arr = sorted(nums)
-        for i in range(1, len(nums), 2): nums[i] = arr.pop() 
-
-        for i in range(0, len(nums), 2): nums[i] = arr.pop() 
+        for i in range(1, len(nums), 2):
+            nums[i] = arr.pop() 
+        for i in range(0, len(nums), 2):
+            nums[i] = arr.pop() 
                
             
             
