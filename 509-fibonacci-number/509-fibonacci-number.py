dfdb68b412a1d081b5c3a8dc2890c514cf4e5964
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-05-17 23:11:39 +0300

    Time: 43 ms (57.98%), Space: 13.8 MB (55.55%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,12 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
-        dp ={}
+        @lru_cache
         def helper(n):
             if n==0 or n==1:
                 return n
-            if n in dp:
-                return dp[n]
-            x = helper(n-1) + helper(n-2)
-            dp[n] =x
-            return x
+            return  helper(n-1) + helper(n-2)
         return helper(n)
+           
