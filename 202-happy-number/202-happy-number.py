94855bbd14a62749180b96e560b3699a3222927d
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-05-25 09:56:55 +0300

    Time: 46 ms (54.32%), Space: 13.7 MB (97.07%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,8 +1,8 @@
 from collections import Counter
 class Solution:
     def isHappy(self, n: int) -> bool:
-        s = { 1 }
-        while n not in s:
-            s.add(n)
+        num_dic = { 1:1 }
+        while n not in num_dic:
+            num_dic[n] = 1
             n = sum(i **2 for i in map(int, str(n)))
         return n == 1
