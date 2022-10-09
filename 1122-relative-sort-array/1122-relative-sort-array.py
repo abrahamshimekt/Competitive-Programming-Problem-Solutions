6d825f9621e6a92b5c5d90b258b10a6daf8001d5
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-10-09 11:28:04 +0300

    Time: 72 ms (47.12%), Space: 14 MB (40.46%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,18 +1,18 @@
+from collections import Counter
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         arr3 = []
+        arr4 = Counter(arr2)
         i = 0
         while i < len(arr1):
-            if arr1[i] not in arr2:
+            if arr1[i] not in arr4:
                 arr3.append(arr1.pop(i))
             else:
                 i +=1
         for num in arr2:
             if num in arr1:
                 arr1.remove(num)
-        
         arr3 = sorted(arr3)
-        
         for num in arr1:
             arr2.insert(arr2.index(num),num)
         for num in arr3:
