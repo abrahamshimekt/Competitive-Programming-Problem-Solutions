af72c845acd1c3f9112103e7d15e1690679832fb
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-10-09 11:15:12 +0300

    Time: 78 ms (34.49%), Space: 14.1 MB (40.46%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        arr3 = []
+        i = 0
+        while i < len(arr1):
+            if arr1[i] not in arr2:
+                arr3.append(arr1.pop(i))
+            else:
+                i +=1
+        for num in arr2:
+            if num in arr1:
+                arr1.remove(num)
+        
+        arr3 = sorted(arr3)
+        
+        for num in arr1:
+            arr2.insert(arr2.index(num),num)
+        for num in arr3:
+            arr2.append(num)
+        return arr2
+        
