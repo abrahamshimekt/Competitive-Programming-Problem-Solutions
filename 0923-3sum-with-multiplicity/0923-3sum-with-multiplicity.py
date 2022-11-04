ca5036150937a4a27e7a89f1c4716ecaaac3a4d0
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-11-04 14:37:40 +0300

    Time: 140 ms (82.47%), Space: 14.1 MB (24.68%) - LeetHub

--- 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.py
+++ 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.py
@@ -1,25 +1,25 @@
 class Solution:
     def threeSumMulti(self, arr: List[int], target: int) -> int:
             arr.sort()
-            cnt = Counter(arr)  
+            freq= Counter(arr)  
             res, i, l = 0, 0, len(arr)
             while i < l:  
                 j, k = i, l-1 
                 while j < k:  
                     if arr[i]+arr[j]+arr[k] < target:
-                        j += cnt[arr[j]]
+                        j += freq[arr[j]]
                     elif arr[i]+arr[j]+arr[k] > target:
-                        k -= cnt[arr[k]]
+                        k -= freq[arr[k]]
                     else:  
                         if arr[i] != arr[j] != arr[k]: 
-                            res += cnt[arr[i]]*cnt[arr[j]]*cnt[arr[k]]
+                            res += freq[arr[i]]*freq[arr[j]]*freq[arr[k]]
                         elif arr[i] == arr[j] != arr[k]:  
-                            res += cnt[arr[i]]*(cnt[arr[i]]-1)*cnt[arr[k]]//2 
+                            res += freq[arr[i]]*(freq[arr[i]]-1)*freq[arr[k]]//2 
                         elif arr[i] != arr[j] == arr[k]: 
-                            res += cnt[arr[i]]*cnt[arr[j]]*(cnt[arr[j]]-1)//2 
+                            res += freq[arr[i]]*freq[arr[j]]*(freq[arr[j]]-1)//2 
                         else:
-                            res += cnt[arr[i]]*(cnt[arr[i]]-1)*(cnt[arr[i]]-2)//6 
-                        j += cnt[arr[j]]
-                        k -= cnt[arr[k]]
-                i += cnt[arr[i]]  
+                            res += freq[arr[i]]*(freq[arr[i]]-1)*(freq[arr[i]]-2)//6 
+                        j += freq[arr[j]]
+                        k -= freq[arr[k]]
+                i += freq[arr[i]]  
             return res%1000000007
