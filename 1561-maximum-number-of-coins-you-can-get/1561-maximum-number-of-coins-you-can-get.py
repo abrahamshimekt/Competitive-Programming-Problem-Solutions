06130d47a56e7390c7db1a11333d42f46c2a3bbd
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-11-29 00:35:44 +0300

    Time: 662 ms (87.74%), Space: 26.8 MB (26.56%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -4,17 +4,17 @@ class Solution:
         backT = n//3
         piles.sort()
         i = n-1
-        my_score = piles[-2]
+        my_coins = piles[-2]
         while i >=0:
             if i == n-1:
                 i -=2
                 backT -=1
             elif backT > 0:
-                my_score += piles[i-1]
+                my_coins += piles[i-1]
                 i -= 2
                 backT -=1
             else:
                 break
-        return my_score
+        return my_coins
                 
         
