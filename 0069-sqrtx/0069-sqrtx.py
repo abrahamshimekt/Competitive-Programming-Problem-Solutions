2d6394efdfadcff052701a4c9dddaed645c5db6f
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-12-03 20:32:19 +0300

    Time: 8370 ms (5.01%), Space: 13.8 MB (96.34%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x ==1:
+            return x
+        half = x//2
+        for i in range(half+1):
+            if i * i == x:
+                return i
+            elif i*i > x:
+                return i-1
+        return i
+            
+        
