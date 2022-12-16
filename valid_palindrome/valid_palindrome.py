46b38cca7cf79eb844e388fd81a43800dfce7ca1
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-12-16 23:07:03 +0300

    valid palindrome

---
+++ valid_palindrome/valid_palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s)==0:
+            return True
+        l,r =0,len(s)-1
+        while l < r:
+            if not s[l].isalnum():
+                l+=1
+            elif  not s[r].isalnum():
+                r -=1
+            elif s[l].lower() != s[r].lower():
+                return False
+            else:
+                l +=1
+                r -=1
+        return True
