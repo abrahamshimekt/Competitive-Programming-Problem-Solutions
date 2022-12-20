7067b41901ef1efaeac7d4b311b13a1124b1d2c4
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-12-20 21:54:08 +0300

    largest perimeter

---
+++ largest_permiter/largest_perimeter.py
@@ -0,0 +1,10 @@
+ def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        max_perimeter  = 0
+        i = 0
+        while i + 3 <= len(nums):
+            curr_triangle = nums[i:i + 3]
+            if curr_triangle[0] + curr_triangle[1] > curr_triangle[2]:
+                max_perimeter = max(max_perimeter,sum(curr_triangle))
+            i +=1
+        return max_perimeter
