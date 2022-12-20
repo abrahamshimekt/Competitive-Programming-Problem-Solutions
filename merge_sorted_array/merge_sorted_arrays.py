0981304682b3a1ecb3ccd5e809b3548ca88c94e0
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-12-20 21:32:18 +0300

    merge two sorted arrays

---
+++ merge_sorted_array/merge_sorted_arrays.py
@@ -0,0 +1,17 @@
+ def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        k = n + m -1
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[k] = nums1[m-1]
+                m -=1
+            else:
+                nums1[k] = nums2[n-1]
+                n -=1
+            k -=1
+        while n > 0:
+            nums1[k] = nums2[n-1]
+            k -=1
+            n -=1
