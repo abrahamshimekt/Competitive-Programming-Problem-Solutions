44aee858b5d93a72ac71968ba055923577c277b3
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-12-21 15:20:05 +0300

    alien dictionary

---
+++ alien_dictionary/alien_dictinar.py
@@ -0,0 +1,14 @@
+ def isAlienSorted(self, words: List[str], order: str) -> bool:
+        alphabets = {}
+        for index,ch in enumerate(order):
+            alphabets[ch] = index
+        words_list = []
+        for word in words:
+            word_cur = []
+            for i in range(len(word)):
+                word_cur.append(alphabets[word[i]])
+            words_list.append(word_cur)
+        for j in range(len(words_list)-1):
+            if words_list[j] > words_list[j+1]:
+                return False
+        return True
