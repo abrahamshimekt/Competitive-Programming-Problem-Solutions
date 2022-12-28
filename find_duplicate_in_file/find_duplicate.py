f60337f717604189a9e4ca815434abb8cee7b8fd
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-12-28 11:30:45 +0300

    check content duplication

--- find_duplicate_in_file/find_duplicate.py
+++ find_duplicate_in_file/find_duplicate.py
@@ -1,10 +1,20 @@
 def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+
         length = len(paths)
         content_duplicate = {}
+
+        # Each time chope the ith source by white space 
+        # and find the root path
         for index in range(length):
             path = paths[index].split()
             root = path[0]
             path_length = len(path)
+
+            # then concatenate the ith file path to the root path
+            # and parse the content of the file by spliting the file using '('
+            # add the content of the file as a key and add the paths in a list as a value 
+            # inorder to check the duplication of content
+
             for indexx in range(1,path_length):
                 curr_path = f'{root}/{path[indexx]}'.split('(')
                 content = curr_path[-1]
@@ -12,8 +22,12 @@ def findDuplicate(self, paths: List[str]) -> List[List[str]]:
                     content_duplicate[content] =[curr_path[0]]
                 else:
                      content_duplicate[content].append(curr_path[0])
+
+        
+
+        # for each content if the contents is duplicated, add it the duplicate files list 
+
         duplicate_files =[]
-        print(content_duplicate)
         for content in content_duplicate:
             if len(content_duplicate[content]) >1:
                 duplicate_files.append(content_duplicate[content])
