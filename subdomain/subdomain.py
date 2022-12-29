d322f5e2050132470c555603cfab03931d59697d
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2022-12-29 11:40:17 +0300

    subdomain updated

--- subdomain/subdomain.py
+++ subdomain/subdomain.py
@@ -1,16 +1,23 @@
 def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+
         sumdomains = {}
+        # split each subdomain to get the cp
         for subdomain in cpdomains:
+
             list_subdomain = subdomain.split()
             cp = int(list_subdomain[0])
             list_subdomain = list_subdomain[1].split('.')
+
             for index in range(len(list_subdomain)):
                 if '.'.join(list_subdomain[index:]) not in sumdomains:
                     sumdomains[ '.'.join(list_subdomain[index:])] = cp
                 else:
                      sumdomains[ '.'.join(list_subdomain[index:])] += cp
+
         output = []
+
         for subd in sumdomains:
             cp_domains = f'{str(sumdomains[subd])} {subd}'
             output.append(cp_domains)
+
         return output
