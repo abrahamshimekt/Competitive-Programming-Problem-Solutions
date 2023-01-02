60db2b6d14603e66920328d56c3f9d0b74357b5c
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-01-02 09:51:37 +0300

    add space between words

--- add_spaces/spaces.py
+++ add_spaces/spaces.py
@@ -1,7 +1,15 @@
 def addSpaces(self, s: str, spaces: List[int]) -> str:
+
+        # add the starting of the slicing 
         spaces.insert(0,0)
+        # add the ending of the slicing
         spaces.append(len(s))
+
         sentence = ''
+        # take two consecutive slicing places 
+        # one for starting and one for ending 
+        # time complexity is O(N) and O(N) for space 
+
         for index in range(len(spaces)-1):
             word = s[spaces[index]:spaces[index+1]]
             if index == 0:
