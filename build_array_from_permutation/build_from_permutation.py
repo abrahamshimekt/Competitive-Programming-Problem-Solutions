5d8cf50a1f91f3c92bac40d1a135ec55120018eb
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-01-03 09:07:28 +0300

    build array from permutation

--- build_array_from_permutation/build_from_permutation.py
+++ build_array_from_permutation/build_from_permutation.py
@@ -1,6 +1,10 @@
 def buildArray(self, nums: List[int]) -> List[int]:
+
         length = len(nums)
+
+        # build the array to return the required answer
         answer = [0]*length
+        
         for index in range(length):
             answer[index] = nums[nums[index]]
         return answer
