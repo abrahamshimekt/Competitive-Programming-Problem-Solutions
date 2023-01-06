f0cb62bd27b226651843120a899d5712bbda39d6
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-01-06 12:10:36 +0300

    replace elements in the array in cetain operations

--- replace_element_in_array/replace_element.py
+++ replace_element_in_array/replace_element.py
@@ -1,13 +1,18 @@
 class Solution:
     def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+
         nums_index = {}
+        # store the index of the elements for later retrieval
+
         for index,num in enumerate(nums):
             nums_index[num] = index
         
         for operation in operations:
-
+            # find the index of the element to be replaced
             replaced_num_index = nums_index[operation[0]]
+            # replace the element by operation[i][1]
             nums[replaced_num_index] =operation[1]
+            # store the new number with its index
             nums_index[operation[1]] = replaced_num_index
             
         return nums 
