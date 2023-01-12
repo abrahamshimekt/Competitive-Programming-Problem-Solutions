ea48c880fdac29acc7249f7e2b1fd5f3c8cf339d
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-01-13 01:43:26 +0300

    reverse strings leetcode

---
+++ reverse_string/reverse.py
@@ -0,0 +1,12 @@
+def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s)-1
+        
+        while left < right:
+            # swap the left and the right element
+            s[left],s[right] = s[right],s[left]
+            left +=1
+            right -=1
