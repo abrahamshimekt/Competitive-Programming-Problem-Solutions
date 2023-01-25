ad473b2ac086b16b69410ecfde2fc46e912c24f3
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-01-25 19:28:55 +0300

    pancake sort

--- pancake_sort/pancake2.py
+++ pancake_sort/pancake2.py
@@ -1,12 +1,18 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
+
         target = sorted(arr)
         right = len(arr)
         flips = []
+
+        # any array can be sorted in 2n flips if we bring the larger element to the begining 
+        
         for index in range(right-1,-1,-1):
+
             pivot = arr.index(target[index])
             flips.append(pivot+1)
             arr = arr[pivot::-1] + arr[pivot+1:]
             flips.append(index+1)
             arr = arr[index::-1] + arr[index+1:]
+
         return flips
