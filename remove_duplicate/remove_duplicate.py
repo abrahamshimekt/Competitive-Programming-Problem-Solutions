855fdd3b14fd3f5a37c065c48d92a674faf44e56
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-01-28 21:18:27 +0300

    remove duplicates

--- remove_duplicate/remove_duplicate.py
+++ remove_duplicate/remove_duplicate.py
@@ -4,6 +4,13 @@ class Solution:
         fast = 1
         length = len(nums)
         
+        # the idea of solving this problem is to easy
+        # first think that the physicall reality of what the two pointers you are going to use are
+        # the fast pointer finds the unique element or element that is diffent from the element pointed 
+        # pointed by the slower pointer and whenever we find an element different  it is going to be placed
+        # next to the element pointed by the slow pointer
+        # that is all about 
+
         while fast < length:
             if nums[fast] == nums[slow]:
                 fast +=1   
