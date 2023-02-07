4ba7cf3db4757dd9fedbefa4d87226999a3dcec6
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-02-07 23:40:48 +0300

    maximum k operations

---
+++ max_k_ops/max_k_op2.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left = 0
+        right = len(nums)-1
+        count = 0
+        while left < right:
+            if nums[left] + nums[right] == k:
+               count +=1
+               left +=1
+               right -=1
+            elif nums[left] + nums[right] < k:
+                left +=1
+            else:
+                right -=1
+        return count
+        
+
+        
+
+       
+
+        
