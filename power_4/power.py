d585d5b96ba6eb8e2cb999e447617de887964ac1
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-02-28 21:43:40 +0300

    power 4

---
+++ power_4/power.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n==1:
+            return True
+        elif n %4 != 0 or n == 0 :
+            return False
+        return self.isPowerOfFour(n/4)
+     
+
+        
