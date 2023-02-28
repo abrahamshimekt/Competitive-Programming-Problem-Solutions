a5a9db482de663f4974fdf3785d8c11bd988a0b6
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-02-28 20:09:36 +0300

    x raised to n

--- power_x/power.py
+++ power_x/power.py
@@ -10,7 +10,9 @@ class Solution:
         elif n==0:
             return 1
         elif n < 0:
-            return 1/(self.myPow(x,-n))
+            return 1/(self.myPow(x,-n)) 
+        # we can express the power of x to n as (x**a)**2 if 2a = 0 or x(x**a)**2 
+        # so first we need to find the power of x to a then we square it 
         val = self.myPow(x,n//2)
         if n%2 ==0:
             return val*val
