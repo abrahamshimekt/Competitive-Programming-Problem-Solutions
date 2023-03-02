87ca9a60ae87ac24117cdb4a579af5d074fa63b0
Abraham Shimekt <87600631+abrahamshimekt@users.noreply.github.com>
2023-03-02 17:12:22 +0300

    Time: 204 ms, Memory: 43.1 MB - LeetHub

---
+++ reverse-string/reverse-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverse_s(self,l,r,s):
+        if l >= r:
+            return s[l]
+        s[r],s[l] = s[l],s[r]
+        return self.reverse_s(l+1,r-1,s)
+             
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        return self.reverse_s(0,len(s)-1,s)
+        
