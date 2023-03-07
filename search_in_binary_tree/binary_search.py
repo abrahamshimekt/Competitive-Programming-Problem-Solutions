958dafd56fd2749c459bf85ca310dd4ce5434eab
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-03-07 17:45:18 +0300

    search in binary tree

---
+++ search_in_binary_tree/binary_search.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        elif root.val == val:
+            return root
+        elif val > root.val:
+            return self.searchBST(root.right,val)
+        return self.searchBST(root.left,val)
+        
