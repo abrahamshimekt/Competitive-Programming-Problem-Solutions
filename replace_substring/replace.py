26ec2a3a969898062749fac6fbd9ec5e63c5fcff
Abraham Shimekt <87600631+Abe40@users.noreply.github.com>
2023-03-13 06:24:10 +0300

    replace substring

---
+++ replace_substring/replace.py
@@ -0,0 +1,15 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        N = len(s)
+        freq = Counter(s)
+        min_length_subarray = float("inf")
+        left = 0
+        for right in range(len(s)):
+            freq[s[right]] -=1
+            while left < N and all(N//4 >= freq[char] for char in "WQER"):
+                min_length_subarray = min(min_length_subarray,right-left+1)
+                freq[s[left]] +=1
+                left +=1
+        return min_length_subarray
+
+        
