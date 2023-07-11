26aa34bb2d4d1a9a7c1d4726b8f09aa165870793
Abraham Shimekt <87600631+abrahamshimekt@users.noreply.github.com>
2023-07-11 11:06:16 +0300

    Create permutation.py

---
+++ permutation.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        n = len(nums)
+        def backtrack(path):
+            if len(path) == n:
+                result.append(path[:])
+                return
+            for num in nums:
+                if num not in path:
+                    path.append(num)
+                    backtrack(path)
+                    path.pop()
+        result = []
+        backtrack([])
+        return result
