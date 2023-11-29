f784b43069bf4f59849e93b7bf9526515dca3e55
Abraham Shimekt <87600631+abrahamshimekt@users.noreply.github.com>
2023-11-29 17:21:56 +0300

    Added solution for Invert Binary Tree

---
+++ RevisionBlind75/invert-binary-tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        q = deque()
+        q.append(root)
+        while q:
+            node = q.popleft()
+            node.right,node.left = node.left,node.right
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        return root
+        
+        
